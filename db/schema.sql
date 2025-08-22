3a994cf59074cac4b6422e6009b085e58e9f7443
Eric Grimes <erictgrimes@gmail.com>
2025-08-22 11:25:31 -0700

    added files and schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,30 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS wineries;
+DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username text NOT NULL UNIQUE,
   password text NOT NULL
+  is_admin boolean NOT NULL DEFAULT false
 );
+
+CREATE TABLE wineries (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  address text NOT NULL,
+  is_approved boolean NOT NULL DEFAULT false,
+  photo text
+);
+
+CREATE TABLE reviews (
+  id serial PRIMARY KEY,
+  venue integer NOT NULL,
+  variety integer NOT NULL,
+  pricing integer NOT NULL,
+  staff integer NOT NULL,
+  overall integer NOT NULL,
+  date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  winery_id integer NOT NULL REFERENCES wineries(id) ON DELETE CASCADE
+)
